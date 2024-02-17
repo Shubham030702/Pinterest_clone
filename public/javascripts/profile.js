3072be632bb22353eb5db84ab7d36afc6889ac93
Shubham030702 <dangwalshubham10@gmail.com>
2024-02-17 23:22:52 +0530

    new changes updated

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -13,3 +13,25 @@ document.addEventListener('click', function(e) {
       arrowIcon.style.transform = 'rotate(0deg)';
     }
   });
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("edit");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+ 
