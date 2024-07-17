3055d265f992bd96148cfa9abac51dbf0ef78f9e
Shubham030702 <dangwalshubham10@gmail.com>
2024-07-17 12:16:13 +0530

    Modified session

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ passport.use(new localStrategy(userModel.authenticate()))
 
 // Route to registration page
 router.get('/', function (req, res) {
+  if(isLoggedIn) res.redirect("/feed");
   res.render('index', { error: req.flash('error') });
 });
 
